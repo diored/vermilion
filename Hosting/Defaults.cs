0ea1d386e4d588482bad7aa957c966f1ee154bed
Dio.RED <38877612+diored@users.noreply.github.com>
2024-10-27 11:00:50 +0200

    Add Hosting library and package

---
+++ Hosting/Defaults.cs
@@ -0,0 +1,10 @@
+namespace DioRed.Vermilion.Hosting;
+public static class Defaults
+{
+    public static string JobEventColor { get; set; } = "mediumspringgreen";
+
+    public static string AzureAccountNameConfigurationKey { get; set; } = "Vermilion:AzureTable:AccountName";
+    public static string AzureAccountKeyConfigurationKey { get; set; } = "Vermilion:AzureTable:AccountKey";
+
+    public static TimeSpan ConsoleLoggerTimeZone { get; set; } = default(DateTimeOffset).Offset;
+}
